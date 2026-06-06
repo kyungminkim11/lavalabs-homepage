bc3e6bab17be35d20ad284114a14ae9f3606fa50
경민 <info@lavalabs.co.kr>
2026-06-06 10:01:53 +0900

    Prevent TypeScript source emits

--- src/Chatbot.d.ts
+++
@@ -1,4 +0,0 @@
-import { Locale } from "./content";
-export declare function Chatbot({ locale }: {
-    locale: Locale;
-}): import("react").JSX.Element;
--- src/LavaScene3D.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function LavaScene3D(): import("react").JSX.Element;
