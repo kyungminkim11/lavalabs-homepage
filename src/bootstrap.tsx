65e6b613405ed5bc560c9ca85b279a170c6005fa
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 04:58:49 +0900

    Add the homepage bootstrap

---
+++ src/bootstrap.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomeApp from "./HomeApp";
+import "./styles.css";
+import "./enhancements.css";
+import "./performance.css";
+import "./home-upgrades.css";
+import "./final-upgrades.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HomeApp />
+  </React.StrictMode>
+);
