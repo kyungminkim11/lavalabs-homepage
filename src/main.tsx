d08d0969b62a9278202757b1a479b30fe645c91e
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 04:39:46 +0900

    Use upgraded homepage while preserving legal pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import HomeApp from "./HomeApp";
 import "./styles.css";
 import "./enhancements.css";
 import "./performance.css";
+import "./home-upgrades.css";
+
+const path = window.location.pathname.toLowerCase();
+const showLegalPage = path.startsWith("/terms") || path.startsWith("/privacy");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {showLegalPage ? <App /> : <HomeApp />}
   </React.StrictMode>
 );
