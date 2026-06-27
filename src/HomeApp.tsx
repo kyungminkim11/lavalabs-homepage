6e49f6c6f428f646bc727583d22dda4296e859a3
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 05:56:09 +0900

    Render Unfollow project on homepage

--- src/HomeApp.tsx
+++ src/HomeApp.tsx
@@ -1,1 +1,11 @@
-export { default } from "./HomePage";
+import HomePage from "./HomePage";
+import UnfollowProjectCard from "./UnfollowProjectCard";
+
+export default function HomeApp() {
+  return (
+    <>
+      <HomePage />
+      <UnfollowProjectCard />
+    </>
+  );
+}
