3c6c9dbc427e33414c06eeea32ab822db7faf4c3
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 14:39:43 +0900

    Use consolidated stylesheet entry point

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomeApp from "./HomeApp";
-import "./styles.css";
-import "./enhancements.css";
-import "./performance.css";
-import "./home-upgrades.css";
-import "./final-upgrades.css";
-import "./unfollow-project.css";
-import "./emoseed-project.css";
-import "./mobile-ux.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
